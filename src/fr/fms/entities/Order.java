3db928c94e38c9b5ac7e888b238904fd4620c5c1
Sebastien Allaire <sebastien.allaire.fms@gmail.com>
2022-04-26 15:06:05 +0200

    :tada: Create Order entity

---
+++ src/fr/fms/entities/Order.java
@@ -0,0 +1,51 @@
+package fr.fms.entities;
+
+import java.sql.Date;
+
+public class Order {
+	
+	private int id;
+	private float totalAmount;
+	private Date createdAt;
+	private int idUser;
+	
+	public Order() {};
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public float getTotalAmount() {
+		return totalAmount;
+	}
+	
+	public void setTotalAmount(float totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	public int getIdUser() {
+		return idUser;
+	}
+	
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+	
+	@Override
+	public String toString() {
+		return "[ Id : " + getId() + " | Total : " + getTotalAmount() + " | Date : " + getCreatedAt() + " ]";
+	}
+	
+}
