ed4a13b62a5c001c6a364d7707e85bea21bd8fb9
Sebastien Allaire <sebastien.allaire.fms@gmail.com>
2022-04-26 15:01:31 +0200

    :tada: Create Customer entity

---
+++ src/fr/fms/entities/Customer.java
@@ -0,0 +1,76 @@
+package fr.fms.entities;
+
+public class Customer {
+	
+	private int id;
+	private String name;
+	private String lastname;
+	private String email;
+	private String phone;
+	private String address;
+	private String roles;
+	
+	public Customer() {};
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getLastname() {
+		return lastname;
+	}
+	
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getRoles() {
+		return roles;
+	}
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+	
+	@Override
+	public String toString() {
+		return "[ Prénom : " + getName() + " | Nom : " + getLastname() + " | Email : " + getEmail() + " ]";
+	}
+
+}
