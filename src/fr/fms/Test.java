eb893a4c44efbbbe082c6f40f566cb99e1f96c38
Sebastien Allaire <sebastien.allaire.fms@gmail.com>
2022-04-26 15:38:47 +0200

    :tada: Create BddConnection & config file

---
+++ src/fr/fms/Test.java
@@ -0,0 +1,13 @@
+package fr.fms;
+
+import fr.fms.dao.BddConnection;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		BddConnection.getConnection();
+
+	}
+
+}
---
+++ src/fr/fms/dao/BddConnection.java
@@ -0,0 +1,47 @@
+package fr.fms.dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class BddConnection {
+	
+	private static Connection connexion = null;
+	private static String driver;
+	private static String url;
+	private static String user;
+	private static String pwd;
+
+	/**
+	 * Bloc d'initialisation static :
+	 * S'execute une seule fois au chargement de la classe avant l'appel des contructeurs 
+	 * Ne s'execute pas aux instanciations
+	 */
+	static
+	{
+		Properties props = new Properties();
+		try (FileInputStream fs = new FileInputStream("files/config.properties")) {
+			props.load(fs);
+			driver = props.getProperty("jdbc.driver.class");
+			url = props.getProperty("jdbc.url");
+			user = props.getProperty("jdbc.user");
+			pwd = props.getProperty("jdbc.password");
+			
+			Class.forName(driver);
+			connexion = DriverManager.getConnection(url, user, pwd);
+			
+		} catch (ClassNotFoundException | IOException | SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static Connection getConnection() {
+		System.out.println("Connexion établie");
+		return connexion;
+	}
+
+}
